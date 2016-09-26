fe55574c21c8c4c88d93e7923870248c84d1e0a2
Sam Vickars <vicks201@newschool.edu>
2016-09-26 15:07:34 +0000

    add assignment 4

--- week4/app.js
+++ week4/app.js
@@ -1,35 +1,38 @@
 // IN THE MONGO SHELL: 
-//   CREATE DATABASE citibike AND SWITCH TO IT WITH: 
-//      use citibike
-//   CREATE COLLECTION stations WITH: 
-//      db.createCollection('stations')
-//   QUERY THE ENTIRE stations COLLECTION WITH:
-//      db.stations.find()
-//   COUNT THE NUMBER OF DOCUMENTS IN THE stations COLLECTION WITH:
-//      db.stations.find().count()
+//   CREATE DATABASE meetingsData AND SWITCH TO IT WITH: 
+//      use meetingsData
+//   CREATE COLLECTION meetings WITH: 
+//      db.createCollection('meetings')
+//   QUERY THE ENTIRE meetings COLLECTION WITH:
+//      db.meetings.find()
+//   COUNT THE NUMBER OF DOCUMENTS IN THE meetings COLLECTION WITH:
+//      db.meetings.find().count()
 
 var request = require('request');
 var fs = require('fs');
+var fileNumber = '10';
 
+var meetingsData = JSON.parse(fs.readFileSync("/home/ubuntu/workspace/week3/data/locations" + fileNumber + ".txt"));
+// console.log(meetingsData);
 
-    var addressData = JSON.parse(fs.readFileSync('/home/ubuntu/workspace/week3/data/locations01.txt'));
-    console.log(addressData);
+    // Connection URL
+    var url = 'mongodb://' + process.env.IP + ':27017/meetingsData';
 
-    // // Connection URL
-    // var url = 'mongodb://' + process.env.IP + ':27017/aameetingslocation';
+    // Retrieve
+    var MongoClient = require('mongodb').MongoClient; // npm install mongodb
 
-    // // Retrieve
-    // var MongoClient = require('mongodb').MongoClient; // npm install mongodb
+    MongoClient.connect(url, function(err, db) {
+        if (err) {return console.dir(err);}
 
-    // MongoClient.connect(url, function(err, db) {
-    //     if (err) {return console.dir(err);}
+        var collection = db.collection('meetings');
 
-    //     var collection = db.collection('addresses');
+        // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+        for (var i=0; i < meetingsData.length; i++) {
+            collection.insert({
+                address: meetingsData[i].clean,
+                location: meetingsData[i].latLong,
+                });
+            }
+        db.close();
 
-    //     // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
-    //     for (var i=0; i < addressData.addressBeanList.length; i++) {
-    //         collection.insert(addressData.addressBeanList[i]);
-    //         }
-    //     db.close();
-
-    // }); //MongoClient.connect
+    }); //MongoClient.connect
