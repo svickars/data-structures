515db1f245c661be0f28b271690a2e32fce50c58
Sam Vickars <vicks201@newschool.edu>
2016-10-03 01:39:08 +0000

    update assignment 5

--- week5/week5.js
+++ week5/week5.js
@@ -6,7 +6,11 @@ var async = require('async'); // npm install async
 var apiKey = process.env.GMAKEY; 
 var zoneNumber = '01';
 
+// read zone html files from assignment 1 and location files (using Google Maps API) from assignment 3
 var content = fs.readFileSync('/home/ubuntu/workspace/week1/zones/'+zoneNumber+'.txt');
+var googleContent = fs.readFileSync('/home/ubuntu/workspace/week3/data/locations' + zoneNumber + '.txt');
+
+// empty arrays to store meeting data
 var meetingStreetAddress = [];
 var meetingLocationName = [];
 var meetingGroupName = [];
@@ -14,8 +18,6 @@ var meetingNotes = [];
 var meetingTime = [];
 var meetingType = [];
 var meetingData = [];
-
-var meetingLoc = [];
 
 var $ = cheerio.load(content);
 
@@ -31,32 +33,14 @@ $('tbody').find('tr').each(function(i, elem){
         }
         // only getting the data for *one* meeting time for each group, not sure where to go from here. divide into meetingDay, meetingStartTime, meetingEndTime??
         meetingTime.push($(elem).find('td').eq(1).html().split('<br>')[0].split('\t    <b>').pop().split('<br>').shift().replace('<b>','').replace('</b>','').replace('to</b>','to').trim());
-        meetingType.push($(elem).find('td').eq(1).html().split('<br>')[1].split('</b>').pop().split('<br>').shift());
+        meetingType.push($(elem).find('td').eq(1).html().split('<br>')[1].split('</b>').pop().split('<br>').shift().trim());
     });
-    
-// async.eachSeries(meetingStreetAddress, function(value, callback) {
-//     var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + value + '&key=' + apiKey;
-//     var meetingLocation = new Object;
-//     request(apiRequest, function(err, resp, body) {
-//         if (err) {throw err;}
-//          else {
-//             meetingLocation.clean = JSON.parse(body).results[0].formatted_address;
-//             meetingLocation.latLong = JSON.parse(body).results[0].geometry.location;
-//         }
-//         meetingLoc.push(meetingLocation);
-//     });
-//     setTimeout(callback, 2000);
-// }, function() {
-//     // console.log(meetingLoc); // print results to console
-//     // fs.writeFileSync('/home/ubuntu/workspace/week3/data/locations' + fileNumber +'.txt', JSON.stringify(meetingsData)); // save results to text file
-// });
-    
-    
 
+console.log(meetingTime);
 
-for (var i = 0; i < meetingGroupName.length; i++) {
-        meetingData.push(JSON.stringify({ groupName: meetingGroupName[i], locationName: meetingLocationName[i], streetAddress: meetingStreetAddress[i], meetingNotes: meetingNotes[i], meetingTimes: meetingTime[i], meetingType: meetingType[i]}));
-}
+// for (var i = 0; i < meetingGroupName.length; i++) {
+//         meetingData.push(JSON.stringify({ groupName: meetingGroupName[i], locationName: meetingLocationName[i], streetAddress: JSON.parse(googleContent)[i].clean, lat: JSON.parse(googleContent)[i].latLong.lat, long: JSON.parse(googleContent)[i].latLong.lng, meetingNotes: meetingNotes[i], meetingTimes: meetingTime[i], meetingType: meetingType[i]}));
+// }
 
-console.log(meetingData);
-fs.writeFile('/home/ubuntu/workspace/week5/data/' + zoneNumber + '.txt', meetingData)
+// console.log(meetingData);
+// fs.writeFile('/home/ubuntu/workspace/week5/data/' + zoneNumber + '.json', meetingData)
