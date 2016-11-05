896b09df823b6d3a45b5a1bd2c3c9980d6cf4890
Sam Vickars <vicks201@newschool.edu>
2016-11-05 19:43:21 +0000

    add week9 assignment

--- week9/query.js
+++ week9/query.js
@@ -1,4 +1,5 @@
 var pg = require('pg');
+var fs = require('fs');
 
 // connection string
 var un = process.env.USERNAME; // aws db username
@@ -6,12 +7,9 @@ var pw = process.env.PASSWORD; // aws db password
 var db = process.env.DBNAME; // aws db database name
 var ep = process.env.DBEP; // aws db endpoint
 var conString = "postgres://" + un + ":" + pw + "@" + ep + "/" + db;
-// console.log(conString);
 
-var createTableQuery = "CREATE TABLE superbowl (dateCreated timestamp DEFAULT current_timestamp, force numeric, resistance numeric, fullness varchar(15));"
-var insertIntoQuery = "INSERT INTO bowl VALUES (DEFAULT, 195.15, 7970.98, 'half full');"
-var query = "SELECT * FROM superbowl;"
-var complexQuery = "SELECT sum(amount) as total FROM wham GROUP BY whammy;"
+
+var query = "SELECT * FROM superbowlII ORDER BY dateCreated;"
 
 pg.connect(conString, function(err, client, done) {
     if (err) {
@@ -19,13 +17,13 @@ pg.connect(conString, function(err, client, done) {
     }
 
     client.query(query, function(err, result) {
-        //call `done()` to release the client back to the pool
         done();
 
         if (err) {
             return console.error('error running query', err);
         }
         console.log(result.rows);
+        // fs.writeFileSync('/home/ubuntu/workspace/week9/testOutput.txt', results.rows); // save results to text file
     });
 
 });
